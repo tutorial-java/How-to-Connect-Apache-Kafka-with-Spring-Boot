ccd7f3f2345be041d2780e31e3099280ba8ad5a0
MrR007 <qorri24j@gmail.com>
2024-12-27 20:56:58 +0700

    How to Connect Apache Kafka with Spring Boot

---
+++ src/test/java/com/qorri/kafka/ExampleKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qorri.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
